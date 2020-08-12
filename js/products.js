5ac9749c3c15b249247fa9fd50494910d339007c
idani07 <68952366+idani07@users.noreply.github.com>
2020-08-12 09:23:57 -0300

    Cambio en el listado de productos v2

--- js/products.js
+++ js/products.js
@@ -6,7 +6,15 @@ function showProductsList(array) {
     for (let i = 0; i < array.length; i++) {
         let product = array[i];
 
-        content += "<p>" + product.name + "</p>";
+        content += `<img src="` + product.imgSrc + `" alt="` + product.name + `" + width="150" + style="float:left;margin-right:15px;">` +
+        "<br>" +
+        "<p>" +
+        product.currency + " " +
+        product.cost + "<br>" +
+        product.name + "<br>" +
+        product.description + "<br>" +
+        "</p>" +
+        "<br>" + "<br>";
         document.getElementById('container productos').innerHTML = content;
     }
 }
@@ -15,8 +23,8 @@ function showProductsList(array) {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
             productsArray = resultObj.data;
             showProductsList(productsArray);
         }
