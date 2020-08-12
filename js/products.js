1c87ffc963538b2d4556ab25d7943dd3ffc6d764
idani07 <68952366+idani07@users.noreply.github.com>
2020-08-12 08:10:40 -0300

    Cambio en el listado de productos

--- js/products.js
+++ js/products.js
@@ -1,6 +1,24 @@
+var productsArray = [];
+
+function showProductsList(array) {
+
+    let content = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        content += "<p>" + product.name + "</p>";
+        document.getElementById('container productos').innerHTML = content;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
 });
